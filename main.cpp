d3c186fdf0398fbc52d96e65712a9f87ee743f8a
Rateb BEN MOUSSA <bmr@linux-twor.site>
2010-01-17 16:39:52 +0100

    added some includes

--- main.cpp
+++ main.cpp
@@ -6,14 +6,36 @@
  *
  * This file is going to be hosted in Github.com
  */
-
 #include <stdlib.h>
+#include <iostream>
+#include <string>
 
-/*
- * 
+/*#include <iostream>*/
+#include <vector>
+using namespace std;
+
+/*int main(int argc, char** argv) {
+     vector<int> justavector(5);
+     justavector.push_back(42);
+     cout << "Vector size is " << justavector.size() << endl;
+     return (EXIT_SUCCESS);
+} */
+
+
+/** @brief class Main utilisée pour lancer le programme principal.
+
+    La classe Main affiche un menu qui permet aux clients d'éffectuer des achats à distance.
+    @author BM Rateb
+    @date 21 juin 2009, 14:58
+
+    @param argc - premier parametre
+    @param argv - deuxième parametre
+    @return Int: déterminant ainsi le success ou l'échec de l'éxecution
  */
 int main(int argc, char** argv) {
-
+ vector<int> justavector(5);
+    int s=justavector.size();
+    cout<<"Vector size is " << s << endl;
     return (EXIT_SUCCESS);
 }
 
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -12,14 +12,14 @@ MKDIR=mkdir
 CP=cp
 CCADMIN=CCadmin
 RANLIB=ranlib
-CC=
-CCC=
-CXX=
+CC=gcc
+CCC=g++
+CXX=g++
 FC=
-AS=
+AS=as
 
 # Macros
-CND_PLATFORM=None-Linux-x86
+CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Debug
 CND_DISTDIR=dist
 
@@ -51,16 +51,16 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	${MAKE}  -f nbproject/Makefile-Debug.mk dist/Debug/None-Linux-x86/xmailqt
+	${MAKE}  -f nbproject/Makefile-Debug.mk dist/Debug/GNU-Linux-x86/xmailqt
 
-dist/Debug/None-Linux-x86/xmailqt: ${OBJECTFILES}
-	${MKDIR} -p dist/Debug/None-Linux-x86
+dist/Debug/GNU-Linux-x86/xmailqt: ${OBJECTFILES}
+	${MKDIR} -p dist/Debug/GNU-Linux-x86
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/xmailqt ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
 ${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -I/usr/include/c++/4.4 -I/usr/include/c++/4.4/x86_64-suse-linux -I/usr/include/c++/4.4/backward -I/usr/local/include -I/usr/lib64/gcc/x86_64-suse-linux/4.4/include-fixed -I/usr/lib64/gcc/x86_64-suse-linux/4.4/include -I/usr/x86_64-suse-linux/include -I/usr/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
@@ -68,7 +68,7 @@ ${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r build/Debug
-	${RM} dist/Debug/None-Linux-x86/xmailqt
+	${RM} dist/Debug/GNU-Linux-x86/xmailqt
 
 # Subprojects
 .clean-subprojects:
