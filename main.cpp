b1a674a58d6f5a2c22b8976be5e12940ffb61ae6
Rateb BEN MOUSSA <bmr@linux-twor.site>
2010-01-17 13:55:30 +0100

    added files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: bmr
+ *
+ * Created on January 17, 2010, 1:43 PM
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Linux-x86
+CND_ARTIFACT_NAME_Debug=xmailqt
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Linux-x86/xmailqt
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Linux-x86/package
+CND_PACKAGE_NAME_Debug=xmailqt.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Linux-x86/package/xmailqt.tar
+# Release configuration
+CND_PLATFORM_Release=None-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/None-Linux-x86
+CND_ARTIFACT_NAME_Release=xmailqt
+CND_ARTIFACT_PATH_Release=dist/Release/None-Linux-x86/xmailqt
+CND_PACKAGE_DIR_Release=dist/Release/None-Linux-x86/package
+CND_PACKAGE_NAME_Release=xmailqt.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Linux-x86/package/xmailqt.tar
